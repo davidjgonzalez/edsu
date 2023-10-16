7e4418eaee5f7aab847b603338c7ca39875fbe87
david g <davidjgonzalez@gmail.com>
2023-10-16 13:08:45 -0400

    ARIA fixes

--- blocks/code-tabs/code-tabs.js
+++ blocks/code-tabs/code-tabs.js
@@ -4,56 +4,55 @@ function html(tabs = []) {
   const blockId = `code-tabs__${crypto.randomUUID()}`;
 
   return `
-        <!-- Vertical layout for small screens -->
-        <div class="vertical" data-js-small-screen role="tablist" aria-orientation="vertical" aria-multiselectable="false" aria-label="Code tabs">
-          ${tabs
+        <!-- Vertical accordian layout for small screens -->
+        <div class="vertical" data-js-small-screen>
+
+        ${tabs
             .map((tab, i) => `
                         <button 
-                            data-js-tab="${i}" 
-                            class="tab" 
-                            role="tab"
-                            id="${`${blockId}__vertical__tab__${i}`}"
-                            aria-controls="${`${blockId}__vertical__tabpanel__${i}`}" 
-                            aria-selected="${i === 0}">${tab.title}</button>
-
-                        <div 
-                              data-js-tab-content="${i}" 
+                            id="${`${blockId}__vertical__control__${i}`}"
+                            aria-controls="${`${blockId}__vertical__content__${i}`}" 
+                            aria-expanded="${i === 0}"
+                            class="tab"
+                            data-js-tab="${i}">${tab.title}</button>
+                        <section 
+                              id="${`${blockId}__vertical__content__${i}`}"                               
+                              role="region"                                                            
+                              aria-hidden="${i !== 0}"
                               class="content"
-                              id="${`${blockId}__vertical__tabpanel__${i}`}" 
-                              role="tabpanel" 
-                              aria-hidden="${i !== 0}">
+                              data-js-tab-content="${i}">
                           <div class="description"${tab.description}</div>
                           <div class="code"><pre><code>${tab.code}</code></pre></div>
-                        </div>
+                        </section>
                 `).join("")}
         </div>
           
-        <!-- Horizontal layout for large screens -->
+        <!-- Horizontal tab layout for large screens -->
         <div class="horizontal" data-js-large-screen>
-          <nav class="tabs" role="tablist" aria-orientation="vertical" aria-multiselectable="false" aria-label="Code tabs">
-                  ${tabs
-                    .map(
-                      (tab, i) => `
-                      <button 
-                          class="tab" 
-                          role="tab"
-                          aria-controls="${`${blockId}__horizontal__${i}`}"
-                          data-js-tab="${i}" 
-                          aria-selected="${i === 0}">${tab.title}</button>`
-                        )
-                    .join("")}
+          <nav role="tablist" aria-orientation="vertical" aria-multiselectable="false" class="tabs">
+          ${tabs
+            .map(
+              (tab, i) => `
+              <button                 
+                  id="${`${blockId}__horizontal__control__${i}`}"
+                  role="tab"
+                  aria-controls="${`${blockId}__horizontal__content__${i}`}"                
+                  aria-selected="${i === 0}"
+                  class="tab"
+                  data-js-tab="${i}">${tab.title}</button>`
+                )
+            .join("")}
           </nav>
 
-              ${tabs
-                .map(
-                  (tab, i) => `
-                      <section class="content" 
+              ${tabs.map((tab, i) => `
+                      <section 
+                              id="${`${blockId}__horizontal__content__${i}`}"
                               role="tabpanel"
-                              id="${`${blockId}__horizontal__${i}`}"
-                              data-js-tab-content="${i}"
-                              aria-hidden="${i !== 0}">
-                          <h4 class="title" 
-                              data-js-tab-control="${i}">${tab.title}</h4>
+                              aria-hidden="${i !== 0}"
+                              class="content"
+                              data-js-tab-content="${i}">
+                          <p class="title" 
+                              data-js-tab-control="${i}">${tab.title}</p>
                           <div class="description" 
                                data-js-description>${tab.description}</div>
                           <div class="code" data-js-code>
@@ -83,9 +82,17 @@ function addTabClickListener(block) {
     tab.addEventListener("click", () => {
       // Update the data-js-tab-control elements to have correct aria-selected state        
       const tabId = tab.dataset.jsTab;
-      tabs.forEach((el) => el.setAttribute("aria-selected", false));
+      
+      tabs.forEach((el) => { 
+        el.setAttribute("aria-selected", false);
+        el.setAttribute("aria-expanded", false);
+      });
+
       // Ensure aria-selected="true" on both large/small screen views of this block
-      block.querySelectorAll(`[data-js-tab="${tabId}"]`).forEach((el) => el.setAttribute("aria-selected", true));
+      block.querySelectorAll(`[data-js-tab="${tabId}"]`).forEach((el) => { 
+        el.setAttribute("aria-selected", true); 
+        el.setAttribute("aria-expanded", true);
+      });
     
       // Update all data-js-tab-content to be hidden
       tabContents.forEach((el) => { el.setAttribute("aria-hidden", true); });
